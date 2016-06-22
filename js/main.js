e7872283dc863d65c98d47946f03c9b5ecb8babb
Krzysztof Miąsko <kmiasko@gmail.com>
2016-06-22 16:54:52 +0200

    font-awesome fix, main.js cleanup

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 (function ifee(window, $) {
   'use strict';
 
+  // function used for handling scroll event, checks if event ocurred
+  // every milliseconds, prevents event flood
+
   function debounce(func, wait, immediate) {
     var timeout;
     return function anon() {
@@ -32,24 +35,24 @@
     );
   }
 
+  // called on document DOMContentLoaded event
+
   function load() {
     var hamburger = document.querySelector('.hamburger');
     var nav = document.querySelector('.navbar');
-    var duration = getDocumentHeight() + window.innerHeight;
     var linksList = nav.querySelectorAll('.nav-link');
     var nextArrow = document.querySelector('.next-arrow');
-    var backToTop = document.querySelector('.triangle-up');
-    var i = 0;
-    var j = 0;
     var links = document.querySelectorAll('a[href^="/#"]');
-    var controller = new ScrollMagic.Controller({ duration: duration });
-    var controller2 = new ScrollMagic.Controller();
     var penTemplateSource = document.querySelector('#pen-template').innerHTML;
     var penTarget = document.querySelector('.last-codepens');
     var penPlaceholder = '<header class="codepens-header"><h2>Codepen</h2></header>\n';
-    var pens;
-    var template = Handlebars.compile(penTemplateSource);
     var infoContainerToggleClass = 'pen__details--open';
+    var controller = new ScrollMagic.Controller();
+    var duration = getDocumentHeight() + window.innerHeight;
+    var template = Handlebars.compile(penTemplateSource);
+    var pens;
+    var i = 0;
+    var j = 0;
 
     var hamburgerClicked = function hc() {
       nav.classList.toggle('nav-open');
@@ -62,23 +65,19 @@
     var nextArrowClicked = function nac(event) {
       var skill = document.getElementById('skills');
       event.preventDefault();
-      controller2.scrollTo(skill.offsetTop);
+      controller.scrollTo(skill.offsetTop);
     };
 
     var linkClick = function lcc(event) {
       var id = this.getAttribute('href').slice(2);
       if (id.length > 0) {
         event.preventDefault();
-        controller2.scrollTo(document.getElementById(id).offsetTop);
+        controller.scrollTo(document.getElementById(id).offsetTop);
 
         if (window.history && window.history.pushState) {
           history.pushState('', document.title, '#' + id);
         }
       }
-    };
-
-    var backToTopClicked = function bttc() {
-      controller2.scrollTo(nav.offsetTop);
     };
 
     var scrollHandler = debounce(function cb() {
@@ -90,25 +89,43 @@
       }
     }, 250);
 
-    autosize(document.querySelectorAll('textarea'));
+    var infoState = function infoState(e) {
+      var el = e.currentTarget.parentNode.querySelector('.pen__details');
+      el.classList.toggle(infoContainerToggleClass);
+    };
 
-    controller2.scrollTo(function cb2(newpos) {
-      TweenLite.to(window, 1, { scrollTo: { y: newpos } });
-    });
+    var removeState = function removeState(e) {
+      if (e.target.classList.contains(infoContainerToggleClass)) {
+        e.target.classList.remove(infoContainerToggleClass);
+      }
+    };
 
-    window.removeEventListener('load', load, false);
+    var addEventHandlers = function addEventHandlers(elemList) {
+      var el;
+      for (i = 0; i < elemList.length; i++) {
+        el = elemList[i].querySelector('.pen__more');
+        el.addEventListener('click', infoState);
+        elemList[i].querySelector('.pen__details').addEventListener('mouseleave', removeState);
+      }
+    };
+
+    window.removeEventListener('DOMContentLoaded', load, false);
     window.addEventListener('scroll', scrollHandler);
     hamburger.addEventListener('click', hamburgerClicked, false);
-    if (nextArrow) nextArrow.addEventListener('click', nextArrowClicked, false);
-    backToTop.addEventListener('click', backToTopClicked, false);
+    nextArrow.addEventListener('click', nextArrowClicked, false);
 
     for (i = 0; i < linksList.length; i += 1) {
       linksList[i].addEventListener('click', linkClicked, false);
     }
 
-    new ScrollMagic.Scene({ triggerElement: '#skills' })
-      .setClassToggle('.navbar', 'darken')
-      .addTo(controller);
+    // form textarea auto resize - plugin
+    autosize(document.querySelectorAll('textarea'));
+
+    // smooth scroll
+    controller.scrollTo(function cb2(newpos) {
+      TweenLite.to(window, 1, { scrollTo: { y: newpos } });
+    });
+
 
     if (window.location.pathname === '/') {
       for (j = 0; j < links.length; j += 1) {
@@ -116,26 +133,8 @@
       }
     }
 
-    function infoState(e) {
-      var el = e.currentTarget.parentNode.querySelector('.pen__details');
-      el.classList.toggle(infoContainerToggleClass);
-    }
-
-    function removeState(e) {
-      if (e.target.classList.contains(infoContainerToggleClass)) {
-        e.target.classList.remove(infoContainerToggleClass);
-      }
-    }
-
-    function addEventHandlers(elemList) {
-      var el;
-      for (i = 0; i < elemList.length; i++) {
-        el = elemList[i].querySelector('.pen__more');
-        el.addEventListener('click', infoState);
-        elemList[i].querySelector('.pen__details').addEventListener('mouseleave', removeState);
-      }
-    }
-
+    // load last 6 pens from my codepen using unofficial cpv2api (CORS)
+    // and add them to last-code-pens section
     $.getJSON('http://cpv2api.com/pens/public/kmiasko')
       .done(function apiResp(resp) {
         if (resp.success) {
@@ -154,5 +153,5 @@
       });
   }
 
-  window.addEventListener('load', load, false);
+  window.addEventListener('DOMContentLoaded', load, false);
 }(window, jQuery));
