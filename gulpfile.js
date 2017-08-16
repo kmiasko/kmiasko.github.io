e9f5c63ba9d505354e2423d73e7001baf95e7221
Krzysztof Miąsko <kmiasko@gmail.com>
2017-08-16 19:39:19 +0200

    optimize css, js, html5 video

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,20 @@ var sass = require('gulp-sass');
 var pprint = require('gulp-print');
 var gulpBabel = require('gulp-babel');
 var gulpConcat = require('gulp-concat');
+var gulpCSSO = require('gulp-csso');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
 
 gulp.task('serve', ['sass', 'js'], function serveFiles() {
   'use strict';
-  gulp.watch('./_css/**/*.sass', ['sass']);
+  gulp.watch('./_css/main.sass', ['sass']);
   gulp.watch('./_sass/**/*.sass', ['sass']);
-  gulp.watch('./_js/**/*.js', ['js']);
+  gulp.watch('./_js/*.js', ['js']);
 });
 
 
 gulp.task('sass', function compileSass() {
-  'use strict';
   return gulp
     .src('./_css/main.sass')
     .pipe(pprint())
@@ -24,13 +27,36 @@ gulp.task('sass', function compileSass() {
 });
 
 gulp.task('js', function compileJs() {
-  'use strict';
   return gulp
-    .src(['./_js/**/*.js'])
+    .src(['./_js/*.js'])
     .pipe(pprint())
     .pipe(gulpBabel({ presets: ['env', 'stage-2'] }))
     .pipe(gulpConcat('all.js'))
     .pipe(gulp.dest('./vendor'));
 });
 
-gulp.task('build', ['sass', 'js']);
+gulp.task('optimize-js', function optimizeJs() {
+  return gulp
+    .src('./vendor/all.js')
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./vendor'));
+});
+
+gulp.task('optimize-css', function optimizeCss() {
+  return gulp
+    .src('./vendor/main.css')
+    .pipe(rename('main.min.css'))
+    .pipe(gulpCSSO())
+    .pipe(gulp.dest('./vendor'));
+});
+
+gulp.task('build-js', function buildJs(cb) {
+  return runSequence('js', 'optimize-js', cb);
+});
+
+gulp.task('build-css', function buildJs(cb) {
+  return runSequence('sass', 'optimize-css', cb);
+});
+
+gulp.task('build', ['build-css', 'build-js']);
