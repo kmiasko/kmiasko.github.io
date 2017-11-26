afc43fb401fcf711d8b0e51d25b2e175d148b38f
Krzysztof Miąsko <kmiasko@gmail.com>
2017-11-26 19:33:32 +0100

    clojure compiler -> standard

--- vendor/all.js
+++ vendor/all.js
@@ -144,7 +144,7 @@
     };
 
     var loadCodepens = function loadCodepens() {
-      return fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D'https%3A%2F%2Fcodepen.io%2Fkmiasko%2Fpublic%2Ffeed'%20limit%206&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`).then(function (resp) {
+      return fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D\'https%3A%2F%2Fcodepen.io%2Fkmiasko%2Fpublic%2Ffeed\'%20limit%206&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").then(function (resp) {
         var pens = penList();
         var items = resp.query.results.item;
         for (var _i = 0, len = items.length; _i < len; _i++) {
